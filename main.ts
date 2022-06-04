070fc6cc8bf9cbedff8a6a710d43f8a588f432eb
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-06-03 23:06:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,11 @@ radio.onReceivedString(function (receivedString) {
         basic.showString("X")
     } else if (receivedString == "start") {
         received_start = 1
+        datalogger.log(datalogger.createCV("accel", -2))
     } else if (receivedString == "set") {
         received_set = 1
+        p_accel = input.acceleration(Dimension.Strength)
+        c_accel = input.acceleration(Dimension.Strength)
         datalogger.log(datalogger.createCV("accel", -1))
     }
 })
@@ -23,38 +26,32 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     }
 })
 let c_accel = 0
+let p_accel = 0
 let received_set = 0
 let received_start = 0
 let lane = ""
 let paired = 0
-let accel_arry: number[] = []
 radio.setGroup(11)
 radio.setTransmitSerialNumber(true)
 paired = 0
 lane = ""
-basic.showString("touch to pair")
+basic.showString("pair")
 input.setAccelerometerRange(AcceleratorRange.FourG)
 datalogger.mirrorToSerial(true)
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
 basic.forever(function () {
 	
 })
-control.inBackground(function () {
-    if (received_set == 1) {
-        let list: number[] = []
-        if (Math.abs(input.acceleration(Dimension.Strength) - list[-1]) >= 500) {
-            radio.sendString("movement")
-            music.playTone(262, music.beat(BeatFraction.Whole))
-        }
-    }
-})
 loops.everyInterval(200, function () {
     if (received_set == 1) {
+        p_accel = c_accel
         c_accel = input.acceleration(Dimension.Strength)
-        if (accel_arry.length == 10) {
-            accel_arry.shift()
-        }
-        accel_arry.push(c_accel)
+        datalogger.log(datalogger.createCV("p_accel", p_accel))
         datalogger.log(datalogger.createCV("accel", c_accel))
+        if (Math.abs(c_accel - p_accel) >= 500) {
+            radio.sendString("movement")
+            music.playTone(175, music.beat(BeatFraction.Half))
+            received_set = 0
+        }
     }
 })
