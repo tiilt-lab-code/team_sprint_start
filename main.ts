55fbf9ccb36fc713ade07afe8d83e9cbcafe6320
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-06-03 22:19:52 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ radio.onReceivedString(function (receivedString) {
         basic.showString("X")
     } else if (receivedString == "start") {
         received_start = 1
+    } else if (receivedString == "set") {
+        received_set = 1
+        datalogger.log(datalogger.createCV("accel", -1))
     }
 })
 radio.onReceivedValue(function (name, value) {
@@ -15,11 +18,16 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (paired == 0) {
         basic.showString("X")
         radio.sendString("pair")
+        received_set = 0
+        received_start = 0
     }
 })
+let c_accel = 0
+let received_set = 0
 let received_start = 0
 let lane = ""
 let paired = 0
+let accel_arry: number[] = []
 radio.setGroup(11)
 radio.setTransmitSerialNumber(true)
 paired = 0
@@ -27,6 +35,24 @@ lane = ""
 basic.showString("touch to pair")
 input.setAccelerometerRange(AcceleratorRange.FourG)
 datalogger.mirrorToSerial(true)
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
 basic.forever(function () {
-    datalogger.log(datalogger.createCV("accel", input.acceleration(Dimension.Strength)))
+	
+})
+control.inBackground(function () {
+    if (received_set == 1) {
+        let list: number[] = []
+        if (Math.abs(input.acceleration(Dimension.X) - list[-1]) >= 500) {
+            radio.sendString("movement")
+        }
+    }
+})
+loops.everyInterval(200, function () {
+    if (received_set == 1) {
+        c_accel = input.acceleration(Dimension.Strength)
+        if (accel_arry.length == 10) {
+            accel_arry.shift()
+        }
+        accel_arry.push(c_accel)
+    }
 })
